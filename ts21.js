963fe3658d37ef4932338e982cba93507b218eb8
De'Shawn Minnis <deshawnminnis@gmail.com>
2017-03-21 11:49:36 -0700

    Add files via upload

--- ts21.js
+++ ts21.js
@@ -1,7 +1,7 @@
 // Typescript Arrow Function //
 var Order = function (amount, description) {
-    if (amount === void 0) { amount = 100; }
-    if (description === void 0) { description = "Standing order amount $ "; }
+    if (amount === void 0) { amount = 100; } ///number 
+    if (description === void 0) { description = "Standing order amount $ "; } //string 
     var message = description + amount;
     alert(message);
 };
